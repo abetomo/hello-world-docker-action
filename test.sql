27ca6a7dfa586beb8ae25d7f34f2764b0a0ecae4
abetomo <abe@enzou.tokyo>
2021-03-31 07:00:31 +0900

    update test.sql

--- test.sql
+++ test.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `test_table` (
   `id` int(11) NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `notes` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
