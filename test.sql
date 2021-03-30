2c7d2d7763e1f3e85b853ac72cdc82bff3602eba
abetomo <abe@enzou.tokyo>
2021-03-30 11:42:32 +0900

    add create table

---
+++ test.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `test_table` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
